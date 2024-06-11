55f1933b665cc3c28e97ad0abb098505b5b76b62
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-11 09:04:36 +0530

    Added css and made changes in button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,26 @@
 import { Button as MuiButton } from "@mui/material";
+import { ReactNode } from "react";
 
 interface ButtonProps {
-    label: string;
-    variant: 'outlined' | 'contained' | 'text';
+  label: string;
+  variant?: "outlined" | "contained" | "text";
+  endIcon?: ReactNode;
+  startIcon?: ReactNode;
+  onClick?: (() => void) | ((event: any) => void);
 }
 
 const Button = (props: ButtonProps) => {
-  const { label, variant } = props;
-  return <MuiButton variant={variant}>{label}</MuiButton>;
+  const { label, variant, endIcon, startIcon, onClick } = props;
+  return (
+    <MuiButton
+      variant={variant}
+      endIcon={endIcon}
+      startIcon={startIcon}
+      onClick={onClick}
+    >
+      {label}
+    </MuiButton>
+  );
 };
 
 export default Button;
