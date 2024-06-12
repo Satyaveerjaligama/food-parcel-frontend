9f83bdc7dff56b1287be19d364dc8b6f3b5c0309
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-12 08:15:52 +0530

    Removed stylings added for body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={alexandria.className}>
+      <body>
         <ThemeProvider theme={theme}>
           <ReduxProvider>{children}</ReduxProvider>
         </ThemeProvider>
