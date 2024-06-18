7641b8167a2665f1f6f4d6ea960814ee173f3607
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-06-18 22:42:22 +0530

    Added cart icon and fixed a snack bar issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import { PRODUCT_NAME } from '@/utilities/constants';
 import { pacifico } from '@/utilities/fonts';
 import TextField from './TextField';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
 const Header = () => {
   return (
@@ -22,7 +23,8 @@ const Header = () => {
           />
         </Grid>
         <Grid item sm={4} className="text-right">
-          <AccountCircleRoundedIcon className="text-4xl" />
+          <AccountCircleRoundedIcon className="text-4xl cursor-pointer" />
+          <ShoppingCartRoundedIcon className="text-4xl ml-6 cursor-pointer" />
         </Grid>
       </Grid>
     </Box>
